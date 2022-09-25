6fec663e65bd11bde2827bc10df24ebffa02e5c6
leela khadka <khadkaleela3@gmail.com>
2022-09-25 14:41:15 -0700

    commit tree b79381d1bb5c6a3d416d5f4e35171b8b4cb54c8a

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  attr_accessor :kind, :equilateral, :isosceles, :scalene
+
+  def initialize(side1, side2, side3)
+
+    if (side1 < 0 || side2 < 0 || side3 < 0) || (side1 == 0 && side2 == 0 && side3 == 0) || !((side1+side2)>side3 && (side1+side3)>side2 && (side2+side3)>side1)
+      begin
+        raise TriangleError
+      end
+    end
+
+    if side1 == side2 && side1 == side3 && side2 == side3
+      @kind = :equilateral
+    elsif side1 == side2 || side1 == side3 || side2 == side3
+      @kind = :isosceles
+    else
+      @kind = :scalene
+    end
+  end
+
+
+  class TriangleError < StandardError
+  end
 end
